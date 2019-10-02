e14d6340c8c574f83a6d85271a5493948dcc255e
Amanda Bozzi <amanda.hewitt@gmail.com>
2019-10-02 15:55:02 +0900

    Adds text logging

--- components/features/article/header/default.jsx
+++ components/features/article/header/default.jsx
@@ -18,8 +18,9 @@ const Header = () => {
         outputType !== 'amp' &&
         <span className="text-info">{`News from ${siteProperties.defaultSiteTitle}!`}</span>
       }
-      <h2 className="card-title">{globalContent.headlines.basic}</h2>
-      <h5 className="card-text">{globalContent.subheadlines.basic}</h5>
+      { outputType === 'amp' && <span>Hi Sankei devs! 👋</span>}
+      {/* <h2 className="card-title">{globalContent.headlines.basic}</h2>
+      <h5 className="card-text">{globalContent.subheadlines.basic}</h5> */}
     </header>
   );
 }
